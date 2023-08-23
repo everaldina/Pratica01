db2815c5a659c9ff243a57e18dfbdc87670f7ba1
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-08-23 09:27:50 -0300

    Primeiro commit

---
+++ Pratica01/Projeto1/meuprog1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string nome_de_usuario;
+  
+  cout << "Digite nome do usuario: ";
+  cin >> nome_de_usuario;
+  cout << "Bom dia " << nome_de_usuario << "." << endl;
+  return 0;
+}
