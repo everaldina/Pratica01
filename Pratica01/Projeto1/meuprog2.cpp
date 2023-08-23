a8b7eecc2c95a229194e16e6059b3f3d3a8b32d3
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-08-23 11:30:56 -0300

    modificação meuprog2

--- Pratica01/Projeto1/meuprog2.cpp
+++ Pratica01/Projeto1/meuprog2.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main(){
-    int numA, numB;
+    float numA, numB;
 
-    cout << "Digite um numero inteiro A: ";
+    cout << "Digite um numero real A: ";
     cin >> numA;
-    cout << "Digite um numero inteiro B: ";
+    cout << "Digite um numero real B: ";
     cin >> numB;
     
+    cout << fixed;
+    cout << setprecision(1);
     cout << endl << "Resultado" << endl;
     cout << "Soma = " << numA + numB << endl;
     cout << "Subtração = " << numA - numB << endl;
     cout << "Multiplicação = " << numA * numB << endl;
     if (numB !=0){
-        cout << "Divisao = " << numA / numB;
-        cout << endl << "Resto = " << numA % numB << endl;
+        cout << "Divisao = " << numA / numB << endl;
     }
     else
         cout << "Divisao Invalida" << endl;
